71de2267248413cda5a2d5084fbcd623bdcf47d2
Kevin Brack <brackkevin@gmail.com>
2024-11-01 05:23:02 +0000

    feat: Register battle-locations router in main application

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 import blizzardRouter from './routes/blizzard';
 import warcraftPetsRouter from './routes/warcraftpets';
 import promptsRouter from './routes/prompts';
+import battleLocationsRouter from './routes/battle-locations';
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 app.use('/api/blizzard', blizzardRouter);
 app.use('/api/warcraftpets', warcraftPetsRouter);
 app.use('/api/prompts', promptsRouter);
+app.use('/api/battle-locations', battleLocationsRouter);
 
 app.listen(port, () => {
     console.log(`Server running on port ${port}`);
